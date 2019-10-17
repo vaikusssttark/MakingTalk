f8300bc8d994bb6bf2b771529826e22392464627
vaikusssttark <vaikusssttark@gmail.com>
2019-10-17 14:11:23 +0300

    Initial commit

---
+++ app/src/main/java/site/makingtalk/MainActivity.java
@@ -0,0 +1,14 @@
+package site.makingtalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
